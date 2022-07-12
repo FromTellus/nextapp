fd2c7e19be152f8055c6484a21bfc9354148f4ae
FromTellus <104759082+FromTellus@users.noreply.github.com>
2022-07-12 14:06:00 +0200

    Blah

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,27 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
-}
+// fetch data from mongodbatlas
+// https://docs.mongodb.com/manual/tutorial/query-data/
+
+import { MongoClient } from "mongodb";
+
+const mongodbUri = process.env.MONGODB_URI;
+
+export default async function handler(req, res) {
+  res.status(200);
+  res.send(await results);
+};
+
+
+export async function Products() {
+  const client = await MongoClient.connect(mongodbUri);
+    console.log("connected to mongodb");
+  const db = client.db("products");
+  const yourCollection = db.collection("products");
+  const yourData = await yourCollection.find().toArray();
+  console.log(yourData);
+  client.close();
+  return yourData;
+};
+
+const results =  Products();
