8606af3952071874834f73dda7f0b682edc4f8a7
FromTellus <104759082+FromTellus@users.noreply.github.com>
2022-07-13 14:00:12 +0200

    I lied, now it works.

--- next.config.js
+++ next.config.js
@@ -2,8 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  source: '/api/:hello*',
-  destination: 'https://main--beamish-bublanina-9fca22.netlify.app/products',
 }
 
 module.exports = nextConfig
