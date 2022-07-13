191d9d47e103e93f27b87f9e4ed14ca2384bb12c
FromTellus <104759082+FromTellus@users.noreply.github.com>
2022-07-13 13:54:17 +0200

    Test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -8,13 +8,13 @@ import { MongoClient } from "mongodb";
 const mongodbUri = process.env.MONGODB_URI;
 
 export default async function handler(req, res) {
-  const client = await MongoClient.connect(mongodbUri);
-  console.log("connected to mongodb");
-    const db = client.db("products");
-    const yourCollection = db.collection("products");
-    const yourData = await yourCollection.find().toArray();
-    console.log(yourData);
-    client.close();
-    res.status(200).json(yourData);
+  // const client = await MongoClient.connect(mongodbUri);
+  // console.log("connected to mongodb");
+  //   const db = client.db("products");
+  //   const yourCollection = db.collection("products");
+  //   const yourData = await yourCollection.find().toArray();
+  //   console.log(yourData);
+  //   client.close();
+    res.status(200).json("yourData");
 };
 
